f4f902cfff027a5cca7b9d6a1330c6a359c92b83
Marcela Friptu <friptu.marcela@gmail.com>
2018-04-04 19:44:53 +0200

    Security config modificated to permit all any request

--- src/main/java/com/herokuapp/backend/config/SecurityConfig.java
+++ src/main/java/com/herokuapp/backend/config/SecurityConfig.java
@@ -18,7 +18,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception { http.authorizeRequests()
             .antMatchers( "/").permitAll()
-            .anyRequest().authenticated()
+            .anyRequest().permitAll()
             .and().csrf().disable(); }
 
 
