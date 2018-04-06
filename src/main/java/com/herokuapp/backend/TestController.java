b803f2da5f93d7fd32fe23a0a8c36e5383baaa55
Marcela Friptu <friptu.marcela@gmail.com>
2018-04-06 17:52:24 +0200

    Email sender integrated with backend

--- src/main/java/com/herokuapp/backend/TestController.java
+++ src/main/java/com/herokuapp/backend/TestController.java
@@ -1,11 +1,18 @@
 package com.herokuapp.backend;
 
+import com.herokuapp.backend.email.Email;
+import com.herokuapp.backend.email.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+
 public class TestController {
+
+    @Autowired
+    private EmailService emailService;
 
 
     @GetMapping()
@@ -18,5 +25,16 @@ public class TestController {
     @PreAuthorize("hasAnyRole('CLIENT','DRIVER','CORPORATION')")
     public String display() {
         return "Hello World test!";
+    }
+
+    @GetMapping("email")
+    public String emailSent() {
+        Email email = new Email();
+        email.setTo("friptu.marcela@gmail.com");
+        email.setSubject("Test Email");
+        email.setContent("Test Email - the email service works");
+        email.setProvider("Provider");
+        emailService.send(email);
+        return "Email was sent";
     }
 }
---
+++ src/main/java/com/herokuapp/backend/email/EmailService.java
@@ -0,0 +1,25 @@
+package com.herokuapp.backend.email;
+
+
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import static com.herokuapp.backend.config.Keys.EMAIL_URL;
+
+@Service
+public class EmailService {
+    private RestTemplate restTemplate = new RestTemplate();
+    private Environment environment;
+
+    public EmailService(Environment environment) {
+        this.environment = environment;
+    }
+
+
+    public void send(Email email) {
+        restTemplate.postForObject(environment.getRequiredProperty(EMAIL_URL), email, Object.class);
+    }
+
+
+}
