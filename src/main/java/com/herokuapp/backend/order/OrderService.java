581915f893816fd280da12f2f54677b2c0874839
Filip Andersen <filipandersen@outlook.com>
2018-04-09 20:55:11 +0200

    draft

--- src/main/java/com/herokuapp/backend/order/OrderService.java
+++ src/main/java/com/herokuapp/backend/order/OrderService.java
@@ -1,9 +1,11 @@
 package com.herokuapp.backend.order;
 
 import org.springframework.stereotype.Service;
+
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
+
 import static com.herokuapp.backend.order.OrderStatus.*;
 
 @Service
@@ -81,7 +83,6 @@ public class OrderService {
 
     private void setStatus(OrderEntity orderEntity, OrderStatus status) {
         orderEntity.setStatus(status);
-
     }
 
     private OrderDto toDto(OrderEntity orderEntity) {
