b48d5441e27e0a0541a482aa9e19f858d5620fe9
Filip Andersen <filipandersen@outlook.com>
2018-04-10 21:03:11 +0200

    Arek job fixed

--- src/main/java/com/herokuapp/backend/corporation/CorporationRepository.java
+++ src/main/java/com/herokuapp/backend/corporation/CorporationRepository.java
@@ -7,5 +7,4 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface CorporationRepository extends JpaRepository<CorporationEntity, Integer> {
     DriverEntity findByEmail(String email);
-    DriverEntity findByToken(String token);
 }
