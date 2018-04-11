341098b08df2d52aa7adfad23f582dc1d34eace2
Jaroslaw Surma <jarek.surma@gmail.com>
2018-04-11 20:18:24 +0200

    firebase auth

--- src/main/java/com/herokuapp/backend/auth/AuthController.java
+++ src/main/java/com/herokuapp/backend/auth/AuthController.java
@@ -1,14 +1,8 @@
 package com.herokuapp.backend.auth;
 
-
-import com.herokuapp.backend.errors.Error;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Collections;
-import java.util.List;
-
 
 @RestController()
 public class AuthController {
