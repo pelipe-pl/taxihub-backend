d9ba4b52e6f33190ceaee35de9075c459fef2a5e
Filip Andersen <filipandersen@outlook.com>
2018-05-01 15:34:24 +0200

    drivers list rest controller added.

--- src/main/java/com/herokuapp/backend/driver/DriverRepository.java
+++ src/main/java/com/herokuapp/backend/driver/DriverRepository.java
@@ -3,9 +3,12 @@ package com.herokuapp.backend.driver;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface DriverRepository extends JpaRepository<DriverEntity, Integer> {
     DriverEntity findByEmail(String email);
     DriverEntity getById(Long id);
     Boolean existsByEmail(String email);
+    List<DriverEntity> findAllByCorporation_Id(Long corporationId);
 }
