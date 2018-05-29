acf876661e6cab87730df3391ecf17167ff29796
Filip Andersen <filipandersen@outlook.com>
2018-05-29 18:06:05 +0200

    Souts for email send testing added - FIX.

--- src/main/java/com/herokuapp/backend/email/EmailService.java
+++ src/main/java/com/herokuapp/backend/email/EmailService.java
@@ -17,7 +17,7 @@ public class EmailService {
 
     public void send(Email email) {
         System.out.println("TEST: EmailService send()");
-        System.out.println(EMAIL_URL);
+        System.out.println(environment.getRequiredProperty(EMAIL_URL));
         System.out.println(email);
         restTemplate.postForObject(environment.getRequiredProperty(EMAIL_URL), email, Object.class);
     }
