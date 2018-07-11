384e60c7eee73762cba8aed310100260cfa58e8f
Marcela Friptu <friptu.marcela@gmail.com>
2018-07-11 10:00:32 +0200

    Email usage verification for the entire database was added

---
+++ src/main/java/com/herokuapp/backend/auth/UserRegistrationChecker.java
@@ -0,0 +1,31 @@
+package com.herokuapp.backend.auth;
+
+import com.herokuapp.backend.client.ClientRepository;
+import com.herokuapp.backend.corporation.CorporationRepository;
+import com.herokuapp.backend.driver.DriverRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserRegistrationChecker {
+
+    private final ClientRepository clientRepository;
+    private final CorporationRepository corpRepository;
+    private final DriverRepository driverRepository;
+
+
+    public UserRegistrationChecker(ClientRepository clientRepository, CorporationRepository corpRepository, DriverRepository driverRepository) {
+        this.clientRepository = clientRepository;
+        this.corpRepository = corpRepository;
+        this.driverRepository = driverRepository;
+    }
+
+
+
+    public boolean emailExists(String email) {
+        if (clientRepository.existsByEmail(email)
+                || corpRepository.existsByEmail(email)
+                || driverRepository.existsByEmail(email))
+        { throw new IllegalArgumentException("This e-mail was already used for another registration.");}
+       else  return false;
+    }
+}
--- src/main/java/com/herokuapp/backend/client/ClientService.java
+++ src/main/java/com/herokuapp/backend/client/ClientService.java
@@ -1,6 +1,7 @@
 package com.herokuapp.backend.client;
 
 import com.herokuapp.backend.auth.FirebaseRegistrationService;
+import com.herokuapp.backend.auth.UserRegistrationChecker;
 import com.herokuapp.backend.email.EmailService;
 import org.springframework.stereotype.Service;
 
@@ -12,20 +13,22 @@ public class ClientService {
     private final ClientRepository clientRepository;
     private final FirebaseRegistrationService firebaseRegistrationService;
     private final EmailService emailService;
+    private final UserRegistrationChecker userRegistrationChecker;
 
-    public ClientService(ClientRepository clientRepository, FirebaseRegistrationService firebaseRegistrationService, EmailService emailService) {
+    public ClientService(ClientRepository clientRepository, FirebaseRegistrationService firebaseRegistrationService, EmailService emailService, UserRegistrationChecker userRegistrationChecker) {
         this.clientRepository = clientRepository;
         this.firebaseRegistrationService = firebaseRegistrationService;
         this.emailService = emailService;
+        this.userRegistrationChecker = userRegistrationChecker;
     }
 
     public void add(ClientDto clientDto) throws ExecutionException, InterruptedException {
-        if (!clientRepository.existsByEmail(clientDto.getEmail())) {
+        if (!userRegistrationChecker.emailExists(clientDto.getEmail())) {
             ClientEntity clientEntity = toEntity(clientDto);
             clientRepository.save(clientEntity);
             firebaseRegistrationService.register(clientDto.getEmail(), clientDto.getPassword());
             emailService.sendWelcomeEmail(clientEntity);
-        } else throw new IllegalArgumentException("Client with this e-mail already exists");
+        }
     }
 
     public ClientDto getById(Long id) {
